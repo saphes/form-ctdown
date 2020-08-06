04527dd8c2ad6beeb688ce2549bc1835a27aec39
scintilla4evr <kacper17@outlook.com>
2020-08-06 23:47:42 +0200

    Bye bye, jQuery!!

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 // STOLEN FROM STACKOVERFLOW I AM SO SORRY
 // https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
-function toHHMMSS(tstr) {
-    var sec_num = parseInt(tstr, 10); // don't forget the second param
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+// modified for modern javascript
+function toHHMMSS(sec_num) {
+    let hours   = Math.floor(sec_num / 3600);
+    let minutes = Math.floor((sec_num % 3600) / 60);
+    let seconds = sec_num % 60;
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
+    if (hours   < 10) { hours = hours.toString().padStart(2, "0"); }
+    if (minutes < 10) { minutes = minutes.toString().padStart(2, "0"); }
+    if (seconds < 10) { seconds = seconds.toString().padStart(2, "0"); }
+
     return hours+':'+minutes+':'+seconds;
 }
 
-var time
-var secselapsed = -1;
+// JQUERY IS GONE FOREVER WE CAN NOW CHEER
+const timeContainer = document.querySelector("#hms");
 
-function ready() {
+let time;
+let secselapsed = -1;
+
+fetch("https://worldtimeapi.org/api/timezone/etc/gmt").then(async req => {
     console.log("noxxy woxxy was here");
+    console.log("%c sci was here too and he left disappointed ", "background: #600; color: white; font-weight: 700;");
 
-    var xmlhttp = new XMLHttpRequest();
+    const data = await req.json();
+    time = Date.parse(data.utc_datetime);
 
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            time = Date.parse(JSON.parse(this.responseText).utc_datetime);
-            setTime();
-        }
-    };
-
-    xmlhttp.open("GET", "https://worldtimeapi.org/api/timezone/etc/gmt", true);
-    xmlhttp.send();
-}
+    setTime();
+})
 
 function setTime() {
     setInterval(_=>{
         secselapsed++;
         let endtime = Date.parse("2020-08-09T14:00:00.000000+00:00");
-        let tstr = ""+(((endtime - time)/1000)-secselapsed);
-        if (parseInt(tstr) > 0) {
-            $("#hms").text(toHHMMSS(tstr));
+        let difference = Math.floor(((endtime - time)/1000)-secselapsed);
+
+        if (difference > 0) {
+            timeContainer.textContent = toHHMMSS(difference);
         } else {
-            $("#hms").text("TIME UP!");
+            timeContainer.textContent = "TIME UP!";
         }
     }, 1000);
 }
